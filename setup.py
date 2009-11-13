8a8fb81d9a55377375c080da2124471c87cf9a17
Hiroaki.Kawai <Hiroaki.Kawai@ba37ce5e-9c32-11de-9070-0b7171dc4b6e>
2009-11-13 13:44:34 +0000

    update description

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ c1=Extension('_geohash',
 	sources=['src/geohash.c'],
 	define_macros = [('PYTHON_MODULE',1),])
 
-setup(name='geohash',
+setup(name='python-geohash',
 	version='0.3',
+	description='Fast, accurate python geohashing library',
+	author='Hiroaki Kawai',
 	py_modules=['geohash','quadtree','jpgrid','jpiarea'],
 	ext_modules = [c1]
 )
