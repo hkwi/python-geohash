dcfbcce9034dfa3847d569d777269810c25e7719
hiroaki.kawai <hiroaki.kawai@ba37ce5e-9c32-11de-9070-0b7171dc4b6e>
2010-11-29 11:19:21 +0000

    rewrite C code. prepared for quadtree speedup.

--- trunk/src/geohash.h
+++ trunk/src/geohash.h
@@ -6,10 +6,14 @@ enum {
 	GEOHASH_OK,
 	GEOHASH_NOTSUPPORTED,
 	GEOHASH_INVALIDCODE,
+	GEOHASH_INVALIDARGUMENT,
+	GEOHASH_INTERNALERROR,
+	GEOHASH_NOMEMORY
 };
 
 int geohash_encode(double latitude, double longitude, char* r, size_t capacity);
 int geohash_decode(char* r, size_t length, double *latitude, double *longitude);
+int geo_neighbors(char *hashcode, char* dst, size_t dst_length, int *string_count);
 
 #ifdef __cplusplus
 }
